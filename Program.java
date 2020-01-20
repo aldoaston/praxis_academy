7fa11d3ee94aab6935ceb73d7e43f6db9ead7f08
Aldo Kevindra Rezandy <aldokevindra@gmail.com>
2020-01-20 16:37:29 +0700

    if, loop, dll

---
+++ Program.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Program
+{
+ 	public static void main(String[] args) {
+        int i;
+        for (i=0; i < 3; i++){
+            System.out.println("Ntod");
+        }
+    }
+}
